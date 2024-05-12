0df302690ae47eeef52574a26ea00081774e9612
Mehmet Bebek <bmbebek@gmail.com>
2024-05-12 19:01:09 +0300

    Documentation - enhanced swagger document

--- src/main/java/com/insider/automessagesender/controller/MessageController.java
+++ src/main/java/com/insider/automessagesender/controller/MessageController.java
@@ -3,15 +3,23 @@ package com.insider.automessagesender.controller;
 import com.insider.automessagesender.dto.MessageRequestDto;
 import com.insider.automessagesender.dto.MessageResponseDto;
 import com.insider.automessagesender.service.MessageService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.CollectionUtils;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "Message", description = "Message management APIs")
 @RestController
 @RequestMapping("/api/v1/messages")
 @RequiredArgsConstructor
@@ -20,6 +28,20 @@ public class MessageController {
 
     private final MessageService messageService;
 
+    @Operation(
+            summary = "Create a new Message",
+            description = "Create a new message with proper content and recipientPhoneNumber. There is a phone number validation",
+            tags = {"messages", "post"})
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "201", content = { @Content(
+                            schema = @Schema(),
+                    mediaType = "application/json")}),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Expectation failed for empty or longer messages and/or not proper phone number",
+                    content = {@Content(schema = @Schema())}),
+            @ApiResponse(responseCode = "500", content = {@Content(schema = @Schema())})})
     @PostMapping
     public ResponseEntity<Void> saveMessage(@Valid @RequestBody MessageRequestDto messageRequest) {
         messageService.saveMessage(messageRequest);
@@ -27,8 +49,30 @@ public class MessageController {
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
+    @Operation(
+            summary = "Get Messages by sent(optional)",
+            tags = {"messages", "get", "filter"})
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    content = { @Content(
+                            schema = @Schema(implementation = MessageResponseDto.class),
+                            mediaType = "application/json")}),
+            @ApiResponse(
+                    responseCode = "204",
+                    description = "There are no messages",
+                    content = {@Content(schema = @Schema())}),
+            @ApiResponse(
+                    responseCode = "500",
+                    content = {@Content(schema = @Schema())})})
     @GetMapping
     public ResponseEntity<List<MessageResponseDto>> getMessages(@RequestParam(required = false) Boolean sent) {
-        return ResponseEntity.ok(messageService.getMessages(sent));
+        List<MessageResponseDto> messages = messageService.getMessages(sent);
+
+        if (CollectionUtils.isEmpty(messages)) {
+            return ResponseEntity.noContent().build();
+        }
+
+        return ResponseEntity.ok(messages);
     }
 }
--- src/main/java/com/insider/automessagesender/controller/MessageSchedulerController.java
+++ src/main/java/com/insider/automessagesender/controller/MessageSchedulerController.java
@@ -1,6 +1,12 @@
 package com.insider.automessagesender.controller;
 
 import com.insider.automessagesender.scheduler.MessageScheduledTask;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -9,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Scheduler", description = "Message scheduler management APIs")
 @RestController
 @RequestMapping("/api/v1/scheduler")
 @RequiredArgsConstructor
@@ -17,6 +24,17 @@ public class MessageSchedulerController {
 
     private final MessageScheduledTask messageScheduledTask;
 
+    @Operation(
+            summary = "Enable or disable message send scheduler",
+            tags = {"scheduler", "put"})
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200", content = { @Content(
+                            schema = @Schema(implementation = String.class),
+                    mediaType = "application/json")}),
+            @ApiResponse(
+                    responseCode = "500",
+                    content = {@Content(schema = @Schema())})})
     @PutMapping
     public ResponseEntity<String> changeMessageSendStatus(@RequestParam boolean enabled) {
         messageScheduledTask.setEnabled(enabled);
